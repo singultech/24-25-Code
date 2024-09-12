0a8b626dc49360479c09d1d409097cc4490a156c
TotalSimplicity <leonardo.kulon@gmail.com>
2024-09-12 18:35:22 -0400

    repair pipeline & create sample detection test opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dev/SampleDetectionDebugger.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.dev;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvWebcam;
+import org.firstinspires.ftc.teamcode.utils.SampleDetectionPipeline;
+
+@TeleOp(name = "Vision Detection OpMode")
+public class SampleDetectionDebugger extends LinearOpMode {
+
+    OpenCvCamera webcam;
+    SampleDetectionPipeline pipeline;
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        pipeline = new SampleDetectionPipeline(telemetry);
+        webcam.setPipeline(pipeline);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(640, 480);  // Resolution can be adjusted
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera error", errorCode);
+            }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Display detection results on telemetry
+            telemetry.addData("Items Detected", pipeline.locatedItems.size());
+            telemetry.update();
+        }
+    }
+}
