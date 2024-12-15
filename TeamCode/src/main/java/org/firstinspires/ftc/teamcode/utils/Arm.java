80e99b3987ec33129b3e17c383ed47ea0ad4111a
TotalSimplicity <leonardo.kulon@gmail.com>
2024-12-15 10:31:05 -0500

    create arm class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Arm.java
@@ -1,5 +1,18 @@
 package org.firstinspires.ftc.teamcode.utils;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Arm {
+    private final Servo leftServo;
+    private final Servo rightServo;
+
+    public Arm(){
+        leftServo = hardwareMap.servo.get("leftFlip");
+        rightServo = hardwareMap.servo.get("rightFlip");
+        rightServo.setDirection(Servo.Direction.REVERSE);
+    }
+
 
 }
