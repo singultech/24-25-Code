a621346b424fdb63bd07dcdd142522085019dcec
Leonardo <leonardo.kulon@gmail.com>
2025-02-01 13:16:38 -0500

    diffy test changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dev/ManualDiffyTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.dev;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.utils.Diffy;
+import org.firstinspires.ftc.teamcode.utils.GamepadPair;
+
+@TeleOp(name = "Manual Diffy Test", group = "Dev")
+public class ManualDiffyTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        GamepadPair gamepads = new GamepadPair(gamepad1, gamepad2);
+        Diffy diffy = new Diffy(hardwareMap);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            gamepads.copyStates();
+
+            double verticalPower = -gamepads.joystickValue(1, "left", "y");
+            double rotationPower = gamepads.joystickValue(1, "right", "x");
+
+            double leftPower = (verticalPower + rotationPower) *0.5;
+            double rightPower = (verticalPower - rotationPower) *0.5;
+
+            diffy.setLeftPower(leftPower);
+            diffy.setRightPower(rightPower);
+
+            telemetry.addData("Right Power", rightPower);
+            telemetry.addData("Left Power", leftPower);
+            telemetry.addData("Right Total Rotation", diffy.getRightTotalRotation());
+            telemetry.addData("Left Total Rotation", diffy.getLeftTotalRotation());
+            telemetry.update();
+        }
+    }
+}
