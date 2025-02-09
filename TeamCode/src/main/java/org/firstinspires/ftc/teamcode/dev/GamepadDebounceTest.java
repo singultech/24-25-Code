ff214db8797cca3755b779ca89c841007ade24e6
Leonardo <leonardo.kulon@gmail.com>
2025-02-09 15:31:06 -0500

    debounce test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dev/GamepadDebounceTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.dev;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.utils.BackArm;
+import org.firstinspires.ftc.teamcode.utils.GamepadPair;
+
+@TeleOp(name = "Back Arm Test", group = "Dev")
+public class GamepadDebounceTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        GamepadPair gamepads = new GamepadPair(gamepad1, gamepad2);
+        int dpadupcount = 0;
+        int dpaddowncount = 0;
+        int circlecount = 0;
+        waitForStart();
+
+        while (opModeIsActive()) {
+            gamepads.copyStates();
+            if (gamepads.isPressed(-1, "dpad_up")) dpadupcount++;
+            if (gamepads.isPressed(-1, "dpad_down")) dpaddowncount++;
+            if (gamepads.isPressed(-1, "circle")) circlecount++;
+
+
+            telemetry.addData("Dpad Up Count", dpadupcount);
+            telemetry.addData("Dpad Down Count", dpaddowncount);
+            telemetry.addData("Circle Count", circlecount);
+            telemetry.update();
+        }
+    }
+}
+
