560ec5e1a6be70be2fae02dfc18574d8ae69ddfd
Vivaanu <85761602+Vivaanu@users.noreply.github.com>
2025-04-07 13:14:30 -0400

    Basic pushing pathing i think

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,11 +13,11 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 11.7)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, -62, Math.toRadians(90)))
-                .splineToSplineHeading(new Pose2d(10, -40, Math.toRadians(0)), Math.toRadians(0))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, -62, Math.toRadians(270)))
+                /*.splineToSplineHeading(new Pose2d(10, -40, Math.toRadians(0)), Math.toRadians(0))
                 .splineToConstantHeading(new Vector2d(36, -44), Math.toRadians(0))
                 .splineToConstantHeading(new Vector2d(49, -10), Math.toRadians(0))
                 .strafeTo(new Vector2d(49, -52))
@@ -25,7 +25,22 @@ public class MeepMeepTesting {
                 .strafeTo(new Vector2d(60, -60))
                 .splineToConstantHeading(new Vector2d(48.25, -58), Math.toRadians(270))
                 .turnTo(Math.toRadians(90))
-                .lineToY(-63.25, new TranslationalVelConstraint(20.0))
+                .lineToY(-63.25, new TranslationalVelConstraint(20.0)) */
+                .lineToY(-36)
+                        .waitSeconds(1)
+                .splineToConstantHeading(new Vector2d(36, -42), Math.toRadians(270))
+                    .turnTo(Math.toRadians(150))
+                    .turnTo(Math.toRadians(270))
+                .strafeTo(new Vector2d(50, -42))
+                .turnTo(Math.toRadians(150))
+                .turnTo(Math.toRadians(270))
+                .strafeTo(new Vector2d(60, -42))
+                .turnTo(Math.toRadians(150))
+                .turnTo(Math.toRadians(270))
+                /*.splineToConstantHeading(new Vector2d(48.25, -50), Math.toRadians(270))
+                .turnTo(Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(48.25, -58), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(10, -36), Math.toRadians(270)) */
                 .build()
         );
 
