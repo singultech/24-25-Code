cf2224ffe13c173589f79dd8f46afa523bd78590
TotalSimplicity <leonardo.kulon@gmail.com>
2025-04-13 06:52:24 -0400

    change target tolerance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SampleAuto.java
@@ -22,8 +22,8 @@ public class SampleAuto extends LinearOpMode {
     public static double y =-43;
     public static double x1 =37;
     public static double y1 =-30;
-    public static double s = 388;
-    public static double roataions = -190;
+    public static double s = 375;
+    public static double roataions = -210;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -49,54 +49,109 @@ public class SampleAuto extends LinearOpMode {
         Actions.runBlocking(
                 drive.actionBuilder(beginPose)
                         .waitSeconds(0.2)
-                        .lineToY(-28)
+                        .lineToY(-26)
                         .build());
         drive.updatePoseEstimate();
         frontArm.setPosition(FrontArm.Position.HANG_SPECIMEN);
         Actions.runBlocking(
                 drive.actionBuilder(drive.pose)
-                        .waitSeconds(0.25)
-                        .lineToY(-41)
+                        .lineToY(-42)
                         .build());
         drive.updatePoseEstimate();
 
         frontGrabber.open();
+        slides.setManualMode(false);
+        slides.setTargetRotation(s);
+//        backAssembly.setTargetPreset(BackAssembly.Preset.FLOOR);
+//        while (!slides.isAtTarget()){
+//            telemetry.addData("slide pos", slides.getRotation());
+//            telemetry.addData("slide target", slides.getTargetRotation());
+//            telemetry.update();
+//        }
         Actions.runBlocking(
                 drive.actionBuilder(drive.pose)
                         .splineToSplineHeading(new Pose2d(10, -40, Math.toRadians(280)), Math.toRadians(280))
                         .strafeTo(new Vector2d(x, y))
                         .build());
         drive.updatePoseEstimate();
-        slides.setManualMode(false);
-        slides.setTargetRotation(s);
-//        backAssembly.setTargetPreset(BackAssembly.Preset.FLOOR);
-        while (!slides.isAtTarget()){
-            telemetry.addData("slide pos", slides.getRotation());
-            telemetry.addData("slide target", slides.getTargetRotation());
-            telemetry.update();
-        }
         slides.forceStopPower();
 
         Actions.runBlocking(
                 drive.actionBuilder(drive.pose)
                         .strafeTo(new Vector2d(x1, y1))
                         .turn(Math.toRadians(roataions))
-                        /*.strafeTo(new Vector2d(43, -38))
-                        .turnTo(Math.toRadians(140))
-                        .turnTo(Math.toRadians(240))
-                        .strafeTo(new Vector2d(50, -38))
-                        .turnTo(Math.toRadians(140))
-
-                         */
                         .build());
         drive.updatePoseEstimate();
-
-        backAssembly.setTargetPreset(BackAssembly.Preset.FOLDED);
-        while (!backAssembly.atTarget()){
-            backAssembly.update();
-            telemetry.addData("diffy", backAssembly);
-            telemetry.update();
+        slides.setTargetRotation(0);
+        frontArm.setPosition(FrontArm.Position.GRAB_FROM_WALL);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToSplineHeading(new Pose2d(39, -16, Math.toRadians(90)), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(60, -11), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(60, -50), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(35.75, -56), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(35.75, -66), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontGrabber.close();
+        frontArm.setPosition(FrontArm.Position.HANG_PREP);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToConstantHeading(new Vector2d(8, -34), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(8, -31), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontArm.setPosition(FrontArm.Position.HANG_SPECIMEN);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToConstantHeading(new Vector2d(8, -40), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontGrabber.open();
+        frontArm.setPosition(FrontArm.Position.GRAB_FROM_WALL);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .strafeTo(new Vector2d(35.5, -60))
+                        .splineToConstantHeading(new Vector2d(35.5, -66), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontGrabber.close();
+        frontArm.setPosition(FrontArm.Position.HANG_PREP);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToConstantHeading(new Vector2d(4, -34), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(4, -31), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontArm.setPosition(FrontArm.Position.HANG_SPECIMEN);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToConstantHeading(new Vector2d(4 , -40), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontGrabber.open();
+        frontArm.setPosition(FrontArm.Position.GRAB_FROM_WALL);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .strafeTo(new Vector2d(36, -60))
+                        .splineToConstantHeading(new Vector2d(36, -66), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontGrabber.close();
+        frontArm.setPosition(FrontArm.Position.HANG_PREP);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToConstantHeading(new Vector2d(2, -34), Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(2, -31), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontArm.setPosition(FrontArm.Position.HANG_SPECIMEN);
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToConstantHeading(new Vector2d(2, -40), Math.toRadians(90))
+                        .build());
+        drive.updatePoseEstimate();
+        frontGrabber.open();
         }
 
     }
-}
