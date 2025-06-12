7d76eafa045bed2ace026bb8ff809bcfc5036ed1
TotalSimplicity <leonardo.kulon@gmail.com>
2025-06-12 14:15:12 -0400

    switch HorizSlidePair to use DC motors instead of RTP Servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HorizSlidePair.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HorizSlidePair.java
@@ -11,64 +11,68 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class HorizSlidePair {
-    private final CRServo leftSlide;
-    private final RTPAxon rightSlide;
+    private final DcMotorEx leftSlide;
+    private final DcMotorEx rightSlide;
     private boolean manualMode;
 
     public HorizSlidePair(HardwareMap hmap) {
-        CRServo leftServo = hmap.crservo.get("rightHorizSlide");
-        CRServo rightServo = hmap.crservo.get("leftHorizSlide");
-        leftServo.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightServo.setDirection(DcMotorSimple.Direction.REVERSE);
-        AnalogInput rightEncoder = hmap.get(AnalogInput.class, "rightHorizSlideEncoder");
-        rightSlide = new RTPAxon(rightServo, rightEncoder, RTPAxon.Direction.REVERSE);
-        rightSlide.setPidCoeffs(0.01, 0.05, 0.0005);
-        rightSlide.setMaxPower(1);
-        leftSlide = leftServo;
+        leftSlide = hmap.get(DcMotorEx.class, "leftHorizSlide");
+        rightSlide = hmap.get(DcMotorEx.class, "rightHorizSlide");
         manualMode = false;
-    }
-
-
-    public void update(){
-        rightSlide.update();
-        leftSlide.setPower(rightSlide.getPower());
+        leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftSlide.setTargetPosition(0);
+        rightSlide.setTargetPosition(0);
+        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void setManualMode(boolean manual){
         manualMode = manual;
-        rightSlide.setRtp(!manual);
+        if(!manual){
+            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        } else {
+            leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
     }
 
     public void setManualPower(double power){
         if(!manualMode) throw new IllegalStateException("Not in manual mode");
 
-        double limitedPower = Math.max(-rightSlide.getMaxPower(), Math.min(rightSlide.getMaxPower(), power));
-        rightSlide.setPower(limitedPower);
+        leftSlide.setPower(power);
+        rightSlide.setPower(power);
     }
 
     public boolean isManualMode(){
         return manualMode;
     }
 
-    public void setTargetRotation(double target){
-        rightSlide.setTargetRotation(target);
+    public void setTargetRotation(int target){
+        rightSlide.setTargetPosition(target);
+        leftSlide.setTargetPosition(target);
     }
 
-    public double getTargetRotation(){
-        return rightSlide.getTargetRotation();
+    public int getTargetRotation(){
+        return rightSlide.getTargetPosition();
     }
 
-    public void changeTargetRotation(double delta){
-        rightSlide.changeTargetRotation(delta);
+    public void changeTargetRotation(int delta){
+        int currentPos = rightSlide.getTargetPosition();
+        int newPos = currentPos + delta;
+        setTargetRotation(newPos);
     }
 
-    public double getRotation(){
-        return rightSlide.getTotalRotation();
+    public int getRotation(){
+        return rightSlide.getCurrentPosition();
     }
     public double getPower(){
         return rightSlide.getPower();
@@ -77,25 +81,18 @@ public class HorizSlidePair {
         rightSlide.setPower(0);
     }
     public boolean isAtTarget(){
-        return rightSlide.isAtTarget();
-    }
-    public void setPidCoeffs(double kP, double kI, double kD){
-        rightSlide.setPidCoeffs(kP, kI, kD);
+        return Math.abs(rightSlide.getTargetPosition()- rightSlide.getCurrentPosition()) < 10;
     }
 
     @NonNull
     public String toString(){
-        return String.format("%s\n Manual Mode: %b", rightSlide.log(), manualMode);
+        return String.format("%s\n Manual Mode: %b\nTarget Pos: %f\nCurrent Pos: %f", manualMode, getTargetRotation(), getRotation());
     }
     @Config
     @TeleOp(name = "Horiz Slides Test", group = "test")
     public static class HorizontalSlidesTest extends LinearOpMode {
         public static int DEGREE_INCREMENT = 15;
         public static double MAX_MANUAL_POWER = 0.5;
-
-        public static double kP = 0.012;
-        public static double kI = 0.02;
-        public static double kD = 0.0005;
         @Override
         public void runOpMode() {
             telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
@@ -107,8 +104,6 @@ public class HorizSlidePair {
 
             while (opModeIsActive()) {
                 gamepads.copyStates();
-                slides.update();
-                slides.setPidCoeffs(kP, kI, kD);
 
 
                 if(gamepads.isPressed("triangle")){
@@ -125,7 +120,6 @@ public class HorizSlidePair {
                 }
 
                 telemetry.addLine(slides.toString());
-                telemetry.addData("Manual Mode", slides.isManualMode() ? "ON" : "OFF");
                 telemetry.update();
             }
         }
